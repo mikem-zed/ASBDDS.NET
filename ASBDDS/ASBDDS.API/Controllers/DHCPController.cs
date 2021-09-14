3a8e3e96b73eaf433faedf1ca67bf7a8b3cce0a3
Insei <goodmobiledevices@gmail.com>
2021-09-14 03:14:44 +0300

    api: controllers: dhcp: add initial version of dhcp server api. via api we can: * Watch leases. * Remove leases that have status - released and not have flag static. * Make leases static/dynamic * Add static lease manualy by mac address and ip address.
    Signed-off-by: Insei <goodmobiledevices@gmail.com>

--- ASBDDS/ASBDDS.API/Controllers/DHCPController.cs
+++ ASBDDS/ASBDDS.API/Controllers/DHCPController.cs
@@ -3,14 +3,128 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using ASBDDS.Shared.Models.Responses;
+using GitHub.JPMikkers.DHCP;
+using Microsoft.AspNetCore.Authorization;
+using DHCPServer = ASBDDS.API.Servers.DHCP.DHCPServer;
 
 namespace ASBDDS.API.Controllers
 {
     [Route("api/admin/dhcp/")]
     [ApiController]
+    [Authorize(Roles = "Admin")]
     public class DHCPController : ControllerBase
     {
+        private DHCPServer _dhcpServer;
+        public DHCPController(DHCPServer dhcpServer)
+        {
+            _dhcpServer = dhcpServer;
+        }
+        [HttpGet("leases/")]
+        public async Task<ApiResponse<List<DHCPLeaseAdminResponse>>> GetLeases()
+        {
+            var resp = new ApiResponse<List<DHCPLeaseAdminResponse>>();
+            try
+            {
+                var leases = new List<DHCPLeaseAdminResponse>();
+                foreach (var lease in _dhcpServer.LeasesManager.GetLeases())
+                {
+                    leases.Add(new DHCPLeaseAdminResponse(lease));
+                }
+                resp.Data = leases;
+            }
+            catch(Exception e)
+            {
+                resp.Status.Code = 1;
+                resp.Status.Message = e.Message;
+            }
+            return resp;
+        }
 
+        [HttpPut("leases/{macAddress}/make-static")]
+        public async Task<ApiResponse> MakeStatic(string macAddress, [FromQuery] string ip)
+        {
+            var resp = new ApiResponse();
+            try
+            {
+                var parsedIp = IPAddress.Parse(ip);
+                var isCorrectIp = !parsedIp.Equals(IPAddress.Any) && _dhcpServer.LeasesManager.Pool.InPool(parsedIp);
+                if (isCorrectIp)
+                {
+                    var lease = _dhcpServer.LeasesManager.Get(Utils.HexStringToBytes(macAddress));
+                    if (lease == null)
+                    {
+                        lease = _dhcpServer.LeasesManager.Create(Utils.HexStringToBytes(macAddress));
+                    }
+                    
+                    lease.Address = parsedIp;
+                    _dhcpServer.LeasesManager.MakeStatic(lease);
+                }
+                else
+                {
+                    resp.Status.Code = 1;
+                    resp.Status.Message = "IP is not correct";
+                }
+            }
+            catch(Exception e)
+            {
+                resp.Status.Code = 1;
+                resp.Status.Message = e.Message;
+            }
+            return resp;
+        }
+        
+        [HttpPut("leases/{macAddress}/make-dynamic")]
+        public async Task<ApiResponse> MakeDynamic(string macAddress)
+        {
+            var resp = new ApiResponse();
+            try
+            {
+                var lease = _dhcpServer.LeasesManager.Get(Utils.HexStringToBytes(macAddress));
+                if (lease is {Static: true})
+                {
+                    _dhcpServer.LeasesManager.MakeDynamic(lease);
+                }
+                else
+                {
+                    resp.Status.Code = 1;
+                    resp.Status.Message = "Lease not found or already dynamic";
+                }
+            }
+            catch(Exception e)
+            {
+                resp.Status.Code = 1;
+                resp.Status.Message = e.Message;
+            }
+            return resp;
+        }
+
+        [HttpDelete("leases/{macAddress}")]
+        public async Task<ApiResponse> DeleteLease(string macAddress)
+        {
+            var resp = new ApiResponse();
+            try
+            {
+                var lease = _dhcpServer.LeasesManager.Get(Utils.HexStringToBytes(macAddress));
+                if (lease is {Static: false})
+                {
+                    _dhcpServer.LeasesManager.Remove(lease);
+                }
+                else
+                {
+                    resp.Status.Code = 1;
+                    resp.Status.Message = "Make sure the lease exists, is released and is not static";
+                }
+                
+            }
+            catch(Exception e)
+            {
+                resp.Status.Code = 1;
+                resp.Status.Message = e.Message;
+            }
+            return resp;
+        }
     }
 }
---
+++ ASBDDS/ASBDDS.Shared/Models/Responses/DHCPLeaseAdminResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using GitHub.JPMikkers.DHCP;
+
+namespace ASBDDS.Shared.Models.Responses
+{
+    public class DHCPLeaseAdminResponse
+    {
+        public string IP { get; set; }
+        public string MacAddress { get; set; }
+        public string Status { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public bool Static { get; set; }
+        public DHCPLeaseAdminResponse() {}
+
+        public DHCPLeaseAdminResponse(DHCPLease lease)
+        {
+            IP = lease.Address.ToString();
+            MacAddress = lease.MacAddress;
+            Status = lease.Status.ToString().ToLower();
+            Start = lease.Start;
+            End = lease.End;
+            Static = lease.Static;
+        }
+    }
+}
